87278bd4547f97445e31942b8067b325065ce2db
HenryTSZ <henrytsz@qq.com>
2020-11-07 18:06:37 +0800

    baidu

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,7 +16,7 @@ module.exports = {
           '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown'
       }
     ],
-    ['meta', { name: 'baidu-site-verification', content: '7F55weZDDc' }], // 百度统计的站点拥有者验证
+    ['meta', { name: 'baidu-site-verification', content: 'code-C3V9fjhTFV' }], // 百度统计的站点拥有者验证
     ['meta', { name: 'theme-color', content: '#11a8cd' }] // 移动浏览器主题颜色
     // ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'}], // 移动端阻止页面缩放
   ],
