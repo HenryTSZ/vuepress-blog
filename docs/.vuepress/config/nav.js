6740eaece0353299f008be269bb2425b6b8d523e
Henry <1309658793@qq.com>
2020-11-07 16:55:49 +0800

    first commit

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,59 @@
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: '前端',
+    link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    items: [
+      { text: 'JavaScript', link: '/pages/3037d9/' }, // 这些link是在相应md文件定义的永久链接。另外，注意结尾是有斜杠的
+      { text: 'Vue', link: '/pages/baff20/' },
+      { text: 'ElementUI', link: '/pages/e29708' }
+    ]
+  },
+  {
+    text: '页面',
+    link: '/ui/',
+    items: [
+      { text: 'HTML', link: '/pages/1b1828/' },
+      { text: 'CSS', link: '/pages/cfc63b/' }
+    ]
+  },
+  {
+    text: '技术',
+    link: '/technology/',
+    items: [
+      { text: '技术文档', link: '/pages/dcd231/' },
+      { text: 'GitHub技巧', link: '/pages/960dcf/' },
+      { text: 'Nodejs', link: '/pages/0758c6/' },
+      { text: 'Chrome', link: '/pages/980078/' },
+      { text: 'Other', link: '/pages/24b988' }
+    ]
+  },
+  {
+    text: '操作系统',
+    link: '/system/',
+    items: [
+      { text: 'Mac', link: '/pages/baa026/' },
+      { text: 'Windows', link: '/pages/368ccf/' },
+      { text: 'VSCode', link: '/pages/6f52fa/' }
+    ]
+  },
+  { text: '关于', link: '/about/' },
+  {
+    text: '收藏',
+    link: '/pages/beb6c0bd8a66cea6/',
+    items: [
+      { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
+      { text: '资源', link: '/pages/eee83a9211a70f9d/' },
+      { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' }
+    ]
+  },
+  {
+    text: '索引',
+    link: '/archives/',
+    items: [
+      { text: '分类', link: '/categories/' },
+      { text: '标签', link: '/tags/' },
+      { text: '归档', link: '/archives/' }
+    ]
+  }
+]
---
+++ docs/index.md
@@ -0,0 +1,35 @@
+---
+home: true
+
+heroImage: /img/cover-logo.png
+
+heroText: Henry's blog
+tagline: 我们在键盘上留下的余温, 也将随时代传递到更远的将来
+
+# bannerBg: auto # auto => 网格纹背景(有bodyBgImg时无背景), 默认 | none => 无 | '大图地址' | background: 自定义背景样式       提示: 如发现文本颜色不适应你的背景时可以到palette.styl修改$bannerTextColor变量
+
+features: # 可选的
+  - title: 前端
+
+    details: JavaScript、ES6、Vue框架等前端技术
+    link: /web/ # 可选
+    imgUrl: /img/web.png # 可选
+
+  - title: 页面
+
+    details: html(5)/css(3)，前端页面相关技术
+    link: /ui/
+    imgUrl: /img/ui.png
+
+  - title: 技术
+
+    details: 技术文档、教程、技巧、总结等文章
+    link: /technology/
+    imgUrl: /img/other.png
+
+# 文章列表显示方式: detailed 默认, 显示详细版文章列表(包括作者、分类、标签、摘要、分页等)| simple => 显示简约版文章列表(仅标题和日期)| none 不显示文章列表
+
+# postList: detailed
+
+# simplePostListLength: 10 # 简约版文章列表显示的文章数量, 默认10.(仅在postList设置为simple时生效)
+---
