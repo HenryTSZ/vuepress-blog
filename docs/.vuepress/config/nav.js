4d569f19eac7761fb5e57ef5b18eb0c01dc2dec0
Henry <1309658793@qq.com>
2020-11-08 09:48:49 +0800

    workflow

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -40,11 +40,11 @@ module.exports = [
   { text: '关于', link: '/about/' },
   {
     text: '收藏',
-    link: '/pages/beb6c0bd8a66cea6/',
+    link: '/pages/beb6c0/',
     items: [
-      { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-      { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-      { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' }
+      { text: '网站', link: '/pages/beb6c0/' },
+      { text: '资源', link: '/pages/eee83a/' },
+      { text: 'Vue资源', link: '/pages/12df8a/' }
     ]
   },
   {
--- utils/baiduPush.js
+++ utils/baiduPush.js
@@ -10,7 +10,11 @@ const urlsRoot = path.join(__dirname, '..', 'urls.txt'); // 百度链接推送�
 const DOMAIN = process.argv.splice(2)[0]; // 获取命令行传入的参数
 
 if (!DOMAIN) {
-  console.log(chalk.red('请在运行此文件时指定一个你要进行百度推送的域名参数，例：node utils/baiduPush.js https://xugaoyi.com'))
+  console.log(
+    chalk.red(
+      '请在运行此文件时指定一个你要进行百度推送的域名参数，例：node utils/baiduPush.js https://tszv.now.sh/'
+    )
+  )
   return
 }
 
@@ -24,7 +28,7 @@ function main() {
   const files = readFileList(); // 读取所有md文件数据
 
   files.forEach( file => {
-    const { data } = matter(fs.readFileSync(file.filePath, 'utf8')); 
+    const { data } = matter(fs.readFileSync(file.filePath, 'utf8'));
 
     if (data.permalink) {
       const link = `\r\n${DOMAIN}${data.permalink}`;
