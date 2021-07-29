c43dc400b7aa1f604f09343cff5d7ea7ccb97ee1
henry <lh69jh@gmail.com>
2021-07-29 16:44:38 +0800

    echarts

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -5,6 +5,7 @@ module.exports = [
     link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       { text: 'JavaScript', link: '/pages/3037d9/' }, // 这些link是在相应md文件定义的永久链接。另外，注意结尾是有斜杠的
+      { text: 'TypeScript', link: '/pages/8086ae/' },
       { text: 'Vue', link: '/pages/baff20/' },
       { text: 'ElementUI', link: '/pages/e29708/' },
       { text: 'React', link: '/pages/710b50/' }
