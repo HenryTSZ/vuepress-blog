a546bedd77d4e1d3f5e58eb82563d621e66e65c2
HenryTSZ <henrytsz@qq.com>
2022-07-10 21:59:12 +0800

    chrome

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -42,9 +42,11 @@ module.exports = [
   {
     text: '键盘侠',
     link: '/keyboardMan/',
-    items: [{ text: 'Vim', link: '/pages/6ccd0f/' }],
-    items: [{ text: 'VSCode', link: '/pages/13d173/' }],
-    items: [{ text: 'Chrome', link: '/pages/a8daa5/' }]
+    items: [
+      { text: 'Vim', link: '/pages/6ccd0f/' },
+      { text: 'VSCode', link: '/pages/13d173/' },
+      { text: 'Chrome', link: '/pages/a8daa5/' }
+    ]
   },
   {
     text: '关于',
